8a69177e3ea4cc6b125e7124bcab0a1ecb40bbc7
William Jordan Conway <wconway2@umd.edu>
2024-11-02 16:26:27 -0400

    update port for heroku

--- server.js
+++ server.js
@@ -7,7 +7,8 @@ import formidable from 'formidable';
 dotenv.config();
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
+
 
 app.use(bodyParser.json());
 app.use(express.static('public'));
